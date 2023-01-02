a5047a33a4118b19b1250d437e3589ee71ba12c4
petmikcz <118632976+petmikcz@users.noreply.github.com>
2023-01-02 23:35:30 +0100

    changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import com.example.demo.model.Message;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@Slf4j
 public class DemoApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    // private static final Logger log =
+    // LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
 
@@ -18,18 +24,22 @@ public class DemoApplication {
     }
 
     public DemoApplication() {
-        // logClassLoares();
+        logInfo();
+    }
+
+    public void logInfo() {
+        Message msg1 = new Message("aaaa");
+        Message msg2 = new Message("aaaa");
+
+        boolean equals1 = msg1.equals(msg2);
+        log.info(String.valueOf(equals1));
+        boolean bool = msg1.hashCode() == msg2.hashCode();
+        log.info(String.valueOf(bool));
+
     }
 
     static {
-        log.info("init static content".toUpperCase());
-    }
-
-    public void logClassLoares() {
-        log.info("ClassLoader Application --- " + DemoApplication.class.getClassLoader());
-        log.info("ClassLoader Logger --- " + Logger.class.getClassLoader());
-        log.info("ClassLoader String --- " + String.class.getClassLoader());
-
+        log.info(" <<< init static content >>>");
     }
 
 }
--- src/main/java/com/example/demo/controller/MsgController.java
+++ src/main/java/com/example/demo/controller/MsgController.java
@@ -1,16 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Message;
 
-@RestController("/dev")
+@RestController()
+@RequestMapping("/dev")
 public class MsgController {
 
-    @GetMapping("/hello")
-    public Message startMessage(@RequestParam(name = "text", defaultValue = "World") String text) {
+    @GetMapping("/message")
+    public Message startMessage(@RequestParam(name = "message", defaultValue = "World") String text) {
         return new Message(String.format("Hello %s!", text));
 
     }
